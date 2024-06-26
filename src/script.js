e7b09e2ec1ec9bb94f924b6d17d5f09bd5fecaa0
mohtar25 <mokhtar2000naj@gmail.com>
2024-06-26 21:06:16 +0200

    getting everything working

--- src/script.js
+++ src/script.js
@@ -24,6 +24,15 @@ app.use(cors());
 
 app.use(express.json());
 app.use(requestLogger);
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
+
+  if (error.name === "CastError") {
+    return response.status(400).send({ error: "malformatted id" });
+  }
+
+  next(error);
+};
 
 const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: "unknown endpoint" });
@@ -51,8 +60,38 @@ app.post("/api/notes", (req, res) => {
   note.save().then((sav) => res.json(sav));
 });
 
-app.use(unknownEndpoint);
+app.get("/api/notes/:id", (req, res, next) => {
+  Note.findById(req.params.id)
+    .then((item) => (item ? res.json(item) : res.status(404).end()))
+    .catch((err) => next(err));
+});
 
+app.delete("/api/notes/:id", (req, res, next) => {
+  Note.findByIdAndDelete(req.params.id)
+    .then((back) => res.status(200).end())
+    .catch((er) => next(er));
+});
+
+app.put("/api/notes/:id", (req, res, next) => {
+  const body = req.body;
+  console.log(body);
+
+  if (!body.content) {
+    return res.status(400).json({ error: "missing content" });
+  }
+
+  const note = {
+    content: body.content,
+    important: body.important,
+  };
+
+  Note.findByIdAndUpdate(req.params.id, note, { new: true })
+    .then((up) => res.json(up))
+    .catch((err) => next(err));
+});
+
+app.use(unknownEndpoint);
+app.use(errorHandler);
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
