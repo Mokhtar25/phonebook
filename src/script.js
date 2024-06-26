aa7ce14257f8b5a609bb31d8b8388091b30d1a16
mohtar25 <mokhtar2000naj@gmail.com>
2024-06-26 20:17:34 +0200

    adding get and create;

--- src/script.js
+++ src/script.js
@@ -1,12 +1,59 @@
-const express = require("express");
-const app = express();
 require("dotenv").config();
+
+console.log(process.env.MONGODB_URL);
+const express = require("express");
+
+const app = express();
+
+app.use(express.static("dist"));
+
+const Note = require("./mongo");
+console.log("note", Note);
+
+const requestLogger = (request, response, next) => {
+  console.log("Method:", request.method);
+  console.log("Path:  ", request.path);
+  console.log("Body:  ", request.body);
+  console.log("---");
+  next();
+};
+
 const cors = require("cors");
 
 app.use(cors());
 
-app.get("/", (res, req) => {
-  req.send("<h3>Hello </h3>");
+app.use(express.json());
+app.use(requestLogger);
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: "unknown endpoint" });
+};
+
+app.get("/api/notes", (req, res) => {
+  Note.find().then((e) => res.json(e));
 });
 
-app.listen(8080);
+app.post("/api/notes", (req, res) => {
+  const data = req.body;
+  console.log(data);
+
+  if (!data.content) {
+    console.log("error");
+
+    res.status(400).json({ error: "content missing" });
+  }
+
+  const note = new Note({
+    important: data.important || false,
+    content: data.content,
+  });
+
+  note.save().then((sav) => res.json(sav));
+});
+
+app.use(unknownEndpoint);
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
